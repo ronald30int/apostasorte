3c4c83a19776343cc92e231377d694948e38f09c
ronald30int <103324912+ronald30int@users.noreply.github.com>
2023-09-15 14:39:06 -0300

    Add files via upload

---
+++ usuario.php
@@ -0,0 +1,16 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<title>HomerPage</title>
+</head>
+
+<body>
+<!--Esta é a página pessoal do usuario-->
+<?php
+
+echo '<header><h1>HomerPage</h1></header>';
+
+?>
+</body>
+</html>
